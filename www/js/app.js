258c56bec360d75c9a2942e91001844ae12b34ff
Krzysztof Cieplucha <krzysztof.cieplucha@gmail.com>
2015-03-04 23:51:51 +0100

    Add list view with simple navigation and rotation.

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('kazimir', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,61 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  
+  .state('menu', {
+    url: '/menu',
+    views: {
+      'container-view': {
+        templateUrl: 'templates/menu.html'
+      }
+    }
+  })
+  
+  .state('new', {
+    url: '/new',
+    views: {
+      'container-view': {
+        templateUrl: 'templates/new.html',
+        controller: 'StreetsController'
+      }
+    }
+  })
+  
+  .state('old', {
+    url: '/old',
+    views: {
+      'container-view': {
+        templateUrl: 'templates/old.html',
+        controller: 'StreetsController'
+      }
+    }
+  })
+  
+  $urlRouterProvider.otherwise('/menu');
+})
+
+.controller('StreetsController', function($scope) {
+  $scope.streets = [
+    { name: 'Krakowska' },
+    { name: 'Plac Nowy' },
+    { name: 'Starowiślna' },
+    { name: 'Podbrzezie' },
+    { name: 'Krakowska' },
+    { name: 'Plac Nowy' },
+    { name: 'Starowiślna' },
+    { name: 'Podbrzezie' },
+    { name: 'Krakowska' },
+    { name: 'Plac Nowy' },
+    { name: 'Starowiślna' },
+    { name: 'Podbrzezie' }
+  ];
+  
+  $scope.rotate = function() {
+    var container = document.getElementsByClassName('container');
+    container = angular.element(container);
+    container.toggleClass('flipped');
+  };
+});
