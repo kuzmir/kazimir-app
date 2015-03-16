1a320cd7382a458fa405b63b11baac77a9de394a
Patrycja Radaczyńska <patrycja.radaczynska@creativestyle.pl>
2015-03-16 18:28:10 +0100

    [MODIFICATION] Display each street detail page.

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('kazimir', ['ionic'])
+var app = angular.module('kazimir', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -55,56 +55,81 @@ angular.module('kazimir', ['ionic'])
     views: {
       'container-view': {
         templateUrl: 'templates/single-old.html',
-        controller: 'StreetsController'
+        controller: 'StreetController'
       }
     }
   })
 
   .state('single-new', {
-    url: '/single-new',
+    url: '/streets/:id',
     views: {
       'container-view': {
         templateUrl: 'templates/single-new.html',
-        controller: 'StreetsController'
+        controller: 'StreetController'
       }
     }
   })
- 
+
   $urlRouterProvider.otherwise('/menu');
 })
 
-.controller('StreetsController', function($scope, $state) {
+app.controller('StreetsController', function($scope, $state, $rootScope) {
   $scope.streets = [
     { id: 0,
       name: 'Krakowska',
       title: 'Ulica Krakowska',
-      description: 'Lorem Ipsum' },
+      description: 'Lorem Ipsum Krakowska' },
     { id:1,
-      name: 'Plac Nowy' },
-    { name: 'Starowiślna' },
-    { name: 'Podbrzezie' },
-    { name: 'Krakowska' },
-    { name: 'Plac Nowy' },
-    { name: 'Starowiślna' },
-    { name: 'Podbrzezie' },
-    { name: 'Krakowska' },
-    { name: 'Plac Nowy' },
-    { name: 'Starowiślna' },
-    { name: 'Podbrzezie' }
+      name: 'Plac Nowy',
+      description: 'Lorem Ipsum Plac Nowy' },
+    { id:2,
+      name: 'Starowiślna',
+      description: 'Lorem Ipsum Starowiślna' },
+    { id:3,
+      name: 'Podbrzezie',
+      description: 'Lorem Ipsum Podbrzezie' },
+    { id: 4,
+      name: 'Krakowska',
+      title: 'Ulica Krakowska',
+      description: 'Lorem Ipsum Krakowska' },
+    { id:5,
+      name: 'Plac Nowy',
+      description: 'Lorem Ipsum Plac Nowy' },
+    { id:6,
+      name: 'Starowiślna',
+      description: 'Lorem Ipsum Starowiślna' },
+    { id:7,
+      name: 'Podbrzezie',
+      description: 'Lorem Ipsum Podbrzezie' },
+    { id: 8,
+      name: 'Krakowska',
+      title: 'Ulica Krakowska',
+      description: 'Lorem Ipsum Krakowska' },
+    { id:9,
+      name: 'Plac Nowy',
+      description: 'Lorem Ipsum Plac Nowy' },
+    { id:10,
+      name: 'Starowiślna',
+      description: 'Lorem Ipsum Starowiślna' },
+    { id:11,
+      name: 'Podbrzezie',
+      description: 'Lorem Ipsum Podbrzezie' },
   ];
   
-  $scope.rotate = function() {
-    var container = document.getElementsByClassName('container');
-    container = angular.element(container);
-    container.toggleClass('flipped');
-  };
-  
+ 
   $scope.singlePostOld = function(street) {
-    $scope.street = street;
-    console.log(street);
+    $rootScope.currentLocationDetails = street;
     $state.go('single-old');
   };
   $scope.singlePostNew = function() {
     $state.go('single-new');
   };
+});
+app.controller('StreetController', function($scope, $rootScope){
+  $scope.street = $rootScope.currentLocationDetails;
+    $scope.rotate = function() {
+      var container = document.getElementsByClassName('container');
+      container = angular.element(container);
+      container.toggleClass('flipped');
+    };
 });
