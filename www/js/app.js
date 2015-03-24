8c3d7f249b5c89cb2f2d1bdc4798aebbcbd4125b
Patrycja Radaczyńska <patrycja.radaczynska@creativestyle.pl>
2015-03-24 18:23:35 +0100

    [MODIFICATION] Adding changes for new part of the app.

--- www/js/app.js
+++ www/js/app.js
@@ -77,43 +77,160 @@ app.controller('StreetsController', function($scope, $state, $rootScope) {
   $scope.streets = [
     { id: 0,
       name: 'Krakowska',
-      title: 'Ulica Krakowska',
-      description: 'Lorem Ipsum Krakowska' },
+      description: {
+        old: {
+          title: 'Old Ulica Krakowska',
+          text: 'Old Lorem Ipsum Krakowska',
+        },
+        after: {
+          title: 'New Ulica Krakowska',
+          text: 'New Lorem Ipsum Krakowska',
+        }
+      },
+    },
     { id:1,
       name: 'Plac Nowy',
-      description: 'Lorem Ipsum Plac Nowy' },
+      description: {
+        old: {
+          title: 'Old Plac Nowy',
+          text: 'Old Lorem Ipsum Plac Nowy',
+        },
+        after: {
+          title: 'New Plac Nowy',
+          text: 'New Lorem Ipsum Plac Nowy',
+        }
+      },
+    },
     { id:2,
       name: 'Starowiślna',
-      description: 'Lorem Ipsum Starowiślna' },
+      description: {
+        old: {
+          title: 'Old Starowiślna',
+          text: 'Old Lorem Ipsum Starowiślna',
+        },
+        after: {
+          title: 'New Starowiślna',
+          text: 'New Lorem Ipsum Starowiślna',
+        }
+      },
+    },
     { id:3,
       name: 'Podbrzezie',
-      description: 'Lorem Ipsum Podbrzezie' },
+      description: {
+        old: {
+          title: 'Old Podbrzezie',
+          text: 'Old Lorem Ipsum Podbrzezie',
+        },
+        after: {
+          title: 'New Podbrzezie',
+          text: 'New Lorem Ipsum Podbrzezie',
+        }
+      },
+    },
     { id: 4,
       name: 'Krakowska',
-      title: 'Ulica Krakowska',
-      description: 'Lorem Ipsum Krakowska' },
+      description: {
+        old: {
+          title: 'Old Krakowska',
+          text: 'Old Lorem Ipsum Krakowska',
+        },
+        after: {
+          title: 'New Krakowska',
+          text: 'New Lorem Ipsum Krakowska',
+        }
+      },
+    },
     { id:5,
       name: 'Plac Nowy',
-      description: 'Lorem Ipsum Plac Nowy' },
+      description: {
+        old: {
+          title: 'Old Plac Nowy',
+          text: 'Old Lorem Ipsum Plac Nowy',
+        },
+        after: {
+          title: 'New Plac Nowy',
+          text: 'New Lorem Ipsum Plac Nowy',
+        }
+      },
+    },
     { id:6,
       name: 'Starowiślna',
-      description: 'Lorem Ipsum Starowiślna' },
+      description: {
+        old: {
+          title: 'Old Starowiślna',
+          text: 'Old Lorem Ipsum Starowiślna',
+        },
+        after: {
+          title: 'New Starowiślna',
+          text: 'New Lorem Ipsum Starowiślna',
+        }
+      },
+    },
     { id:7,
       name: 'Podbrzezie',
-      description: 'Lorem Ipsum Podbrzezie' },
+      description: {
+        old: {
+          title: 'Old Podbrzezie',
+          text: 'Old Lorem Ipsum Podbrzezie',
+        },
+        after: {
+          title: 'New Podbrzezie',
+          text: 'New Lorem Ipsum Podbrzezie',
+        }
+      },
+    },
     { id: 8,
       name: 'Krakowska',
-      title: 'Ulica Krakowska',
-      description: 'Lorem Ipsum Krakowska' },
+      description: {
+        old: {
+          title: 'Old Krakowska',
+          text: 'Old Lorem Ipsum Krakowska',
+        },
+        after: {
+          title: 'New Krakowska',
+          text: 'New Lorem Ipsum Krakowska',
+        }
+      },
+    },
     { id:9,
       name: 'Plac Nowy',
-      description: 'Lorem Ipsum Plac Nowy' },
+      description: {
+        old: {
+          title: 'Old Plac Nowy',
+          text: 'Old Lorem Ipsum Plac Nowy',
+        },
+        after: {
+          title: 'New Plac Nowy',
+          text: 'New Lorem Ipsum Plac Nowy',
+        }
+      },
+    },
     { id:10,
       name: 'Starowiślna',
-      description: 'Lorem Ipsum Starowiślna' },
+      description: {
+        old: {
+          title: 'Old Starowiślna',
+          text: 'Old Lorem Ipsum Starowiślna',
+        },
+        after: {
+          title: 'New Starowiślna',
+          text: 'New Lorem Ipsum Starowiślna',
+        }
+      },
+    },
     { id:11,
       name: 'Podbrzezie',
-      description: 'Lorem Ipsum Podbrzezie' },
+      description: {
+        old: {
+          title: 'Old Podbrzezie',
+          text: 'Old Lorem Ipsum Podbrzezie',
+        },
+        after: {
+          title: 'New Podbrzezie',
+          text: 'New Lorem Ipsum Podbrzezie',
+        }
+      },
+    },
   ];
   
  
@@ -121,9 +238,11 @@ app.controller('StreetsController', function($scope, $state, $rootScope) {
     $rootScope.currentLocationDetails = street;
     $state.go('single-old');
   };
-  $scope.singlePostNew = function() {
+  $scope.singlePostNew = function(street) {
+    $rootScope.currentLocationDetails = street;
     $state.go('single-new');
   };
+  $scope.navTitle='<img class="title-image" src="images/kidsintouchtext.png" />';
 });
 app.controller('StreetController', function($scope, $rootScope){
   $scope.street = $rootScope.currentLocationDetails;
